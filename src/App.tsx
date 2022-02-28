46e1f45c42d9076b380c96f6f15f847a26f3b6ba
Great <greatonosu@gmail.com>
2022-02-28 11:33:25 +0100

    refactor:clean react project removing unnecessary files, codes and create project folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from 'react';
 
-function App() {
+const App: FC = () => {
   return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
-}
+};
 
 export default App;
