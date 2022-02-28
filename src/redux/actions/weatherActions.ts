4da632a893a2bb93d6fdbf058c772152b5f943c2
Great <greatonosu@gmail.com>
2022-02-28 12:56:59 +0100

    feat: create redux actions to handle get weather functionality

---
+++ src/redux/actions/weatherActions.ts
@@ -0,0 +1,36 @@
+import { ThunkAction } from 'redux-thunk';
+import { RootState } from '../store';
+import {
+  GET_WEATHER,
+  SET_LOADING,
+  WeatherAction,
+  WeatherResponse,
+} from '../../interfaces/weather';
+import axios from 'axios';
+
+const defaultWoeid: number = 638242;
+
+export const getWeather = (
+  woeid: number = defaultWoeid
+): ThunkAction<void, RootState, null, WeatherAction> => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.get(
+        `https://www.metaweather.com/api/location/${woeid}`
+      );
+      const responseData: WeatherResponse = response.data;
+      dispatch({
+        type: GET_WEATHER,
+        payload: responseData,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const setLoading = (): WeatherAction => {
+  return {
+    type: SET_LOADING,
+  };
+};
