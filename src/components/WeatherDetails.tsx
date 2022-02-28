bc282fc7bd324267ebe1a5e9d98adff5768235d5
Great <greatonosu@gmail.com>
2022-02-28 14:21:13 +0100

    refactor: function to display wind speed in knots per hour. also rounded up decimal numbers for users to easily read the details

--- src/components/WeatherDetails.tsx
+++ src/components/WeatherDetails.tsx
@@ -6,19 +6,26 @@ interface WeatherDetailsProps {
 }
 
 const WeatherDetails: FC<WeatherDetailsProps> = ({ weather }) => {
+  const roundUpInfo = (info: number): number => {
+    return Math.round(info);
+  };
+  const convertWindSpeed = (windSpeed: number): number => {
+    return roundUpInfo(windSpeed / 1.151);
+  };
+
   return (
     <div>
       <div className='grid grid-cols-2 gap-4 clear-both'>
         <div className='grid grid-cols-1 gap-2 text-center'>
           <p className='font-bold text-5xl text-amber-500 mb-2'>
-            {Math.round(weather.the_temp)}&deg;C
+            {roundUpInfo(weather.the_temp)}&deg;C
           </p>
           <p className='text-gray-600 text-3xl'>{weather.weather_state_name}</p>
         </div>
         <div className='grid grid-cols-1 gap-2 my-2 border-l-2 border-gray-200 px-4'>
           <p className='col-end-2 text-gray-500'>
             <strong>Wind Speed: </strong>
-            {Math.round(weather.wind_speed)}mph
+            {convertWindSpeed(weather.wind_speed)}kph
           </p>
           <p className='text-gray-500'>
             <strong>Humidity: </strong>
@@ -26,11 +33,11 @@ const WeatherDetails: FC<WeatherDetailsProps> = ({ weather }) => {
           </p>
           <p className='text-gray-500'>
             <strong>Min Temp: </strong>
-            {Math.round(weather.min_temp)}&deg;C
+            {roundUpInfo(weather.min_temp)}&deg;C
           </p>
           <p className='text-gray-500'>
             <strong>Max Temp: </strong>
-            {Math.round(weather.max_temp)}&deg;C
+            {roundUpInfo(weather.max_temp)}&deg;C
           </p>
         </div>
       </div>
